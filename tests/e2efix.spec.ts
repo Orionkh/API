f50a502bfe76e532a029aebdf83a8dc399ee30df
Orionkh <avdkhr@gmail.com>
2024-03-21 16:34:11 +0200

    download

--- tests/e2efix.spec.ts
+++ tests/e2efix.spec.ts
@@ -1,14 +1,14 @@
-import { test } from "../fixtures/fixture";
+// import { test } from "../fixtures/fixture";
 
 
-test.describe("e2e Test for hillel2", () => {
-    test("Check first login", async ({loginPage}) => {
-        await loginPage.visit("https://lms.ithillel.ua/auth");
-        await loginPage.login();
-    });
+// test.describe("e2e Test for hillel2", () => {
+//     test("Check first login", async ({loginPage}) => {
+//         await loginPage.visit("https://lms.ithillel.ua/auth");
+//         await loginPage.login();
+//     });
 
-    test("Check first lesson2", async ({homePage}) => {
-        await homePage.checkFirstLesson();
-    });
-});
+//     test("Check first lesson2", async ({homePage}) => {
+//         await homePage.checkFirstLesson();
+//     });
+// });
 
